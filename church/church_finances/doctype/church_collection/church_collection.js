2eae8b9d0eafe458447dd5a6f813bc7b60c41425
meichthys <github.com@meichthys.com>
2025-10-02 05:29:47 +0000

    Add fund balance tracking & transactions

--- church/church_finances/doctype/church_collection/church_collection.js
+++ church/church_finances/doctype/church_collection/church_collection.js
@@ -19,20 +19,57 @@ frappe.ui.form.on("Church Collection", {
 	},
 });
 
-
-
 // Keep Collection `total_amount` up to date when amounts are changed/added
-frappe.ui.form.on("Church Donation", "amount", update_collection_total);
+frappe.ui.form.on("Church Donation", "amount", function(frm, cdt, cdn) {
+    update_collection_total(frm);
+    update_fund_totals(frm);
+});
+
 // Keep Collection `total_amount` up to date when rows are removed from grid
 frappe.ui.form.on("Church Donation", {
-	donations_remove: update_collection_total
+    donations_remove: function(frm) {
+        update_collection_total(frm);
+        update_fund_totals(frm);
+    }
+});
+
+// Also update when fund field changes
+frappe.ui.form.on("Church Donation", "fund", function(frm, cdt, cdn) {
+    update_fund_totals(frm);
 });
 
 // Update Collection `total_amount` with sum of donation amounts
 function update_collection_total(frm) {
-	var total = 0;
-	frm.doc.donations.forEach(function (donation) {
-		total += donation.amount || 0;
-	});
-	frm.set_value("total_amount", total);
+    var total = 0;
+    frm.doc.donations.forEach(function (donation) {
+        total += donation.amount || 0;
+    });
+    frm.set_value("total_amount", total);
+}
+
+// Update fund_totals table based on donations
+function update_fund_totals(frm) {
+    // Clear existing fund_totals before each update
+    frm.clear_table("fund_totals");
+
+    // Calculate totals by fund
+    var fund_totals = {};
+    frm.doc.donations.forEach(function(donation) {
+        if (donation.fund && donation.amount) {
+            if (!fund_totals[donation.fund]) {
+                fund_totals[donation.fund] = 0;
+            }
+            fund_totals[donation.fund] += donation.amount;
+        }
+    });
+
+    // Add rows to fund_totals table
+    Object.keys(fund_totals).forEach(function(fund) {
+        var row = frm.add_child("fund_totals");
+        row.fund = fund;
+        row.total = fund_totals[fund];
+    });
+
+    // Refresh the fund_totals field to show updated data
+    frm.refresh_field("fund_totals");
 }
