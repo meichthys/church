e9641d451e61101a9534ce8baf9a1154caf2666d
meichthys <github.com@meichthys.com>
2025-10-10 04:14:28 +0000

    sort fund transactions by date - descending

--- church/church_finances/doctype/church_fund/church_fund.js
+++ church/church_finances/doctype/church_fund/church_fund.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2025, meichthys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Church Fund", {
-// 	refresh(frm) {
+frappe.ui.form.on('Church Fund', {
+    onload: function(frm) {
+        sort_transactions_by_date(frm);
+    }
+});
 
-// 	},
-// });
+function sort_transactions_by_date(frm) {
+    // Sort descending
+    frm.doc.transactions.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB - dateA;
+    });
+
+    frm.refresh_field('transactions');
+}
