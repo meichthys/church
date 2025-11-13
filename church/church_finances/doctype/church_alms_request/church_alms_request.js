1fbfa9268e47d9d3a38b76c23328c4d255dd9f93
meichthys <github.com@meichthys.com>
2025-11-13 05:02:03 +0000

    only show "Create Expense" icon for Alms when document has been saved (not on new documents)

--- church/church_finances/doctype/church_alms_request/church_alms_request.js
+++ church/church_finances/doctype/church_alms_request/church_alms_request.js
@@ -4,20 +4,21 @@
 frappe.ui.form.on('Church Alms Request', {
     refresh(frm) {
         // Add a custom button that creates an Expense from the Alms Request
-        frm.add_custom_button(__('Create Expense'), function () {
-            frappe.call({
-                method: 'church.church_finances.doctype.church_alms_request.church_alms_request.create_expense',
-                args: {
-                    alms_request_name: frm.doc.name
-                },
-                callback: function () {
-                    frm.reload_doc();
-                }
+        if (!frm.is_new()) {
+            frm.add_custom_button(__('Create Expense'), function () {
+                frappe.call({
+                    method: 'church.church_finances.doctype.church_alms_request.church_alms_request.create_expense',
+                    args: {
+                        alms_request_name: frm.doc.name
+                    },
+                    callback: function () {
+                        frm.reload_doc();
+                    }
+                });
             });
-        });
+        }
     },
 });
-
 
 
 
