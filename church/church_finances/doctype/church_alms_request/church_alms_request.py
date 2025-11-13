59f17d2cff8600aa53aacc257849ab001bd3f73f
meichthys <github.com@meichthys.com>
2025-11-13 04:25:10 +0000

    Add button to Alms Request to create asociated Expense Fixes #83

--- church/church_finances/doctype/church_alms_request/church_alms_request.py
+++ church/church_finances/doctype/church_alms_request/church_alms_request.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, meichthys and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ChurchAlmsRequest(Document):
 	pass
+
+
+@frappe.whitelist()
+def create_expense(alms_request_name):
+	"""Create a Church Expense from the given Alms Request."""
+	alms = frappe.get_doc("Church Alms Request", alms_request_name)
+	# Make sure an expense type and amount are provided
+	if not alms.amount:
+		frappe.throw("⚠️ An amount is required for an expense to be created.")
+	if not alms.expense_type:
+		frappe.throw("⚠️ An expense type is required for an expense to be created.")
+	expense = frappe.new_doc("Church Expense")
+	expense.amount = alms.amount
+	expense.notes = f"Church Alms Request: {alms.name}"
+	expense.type = frappe.db.get_value("Church Expense Type", {"name": "Alms"}, "name")
+	expense.date = frappe.utils.now()
+	expense.insert(ignore_permissions=True)
+	frappe.msgprint(f"✅ {expense.type} expense created.")
+	expense.submit()
