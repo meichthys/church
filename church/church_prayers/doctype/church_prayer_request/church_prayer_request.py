522a101759b4e4476f6d5f8a4294e6a10871982f
meichthys <github.com@meichthys.com>
2025-11-16 04:17:49 +0000

    Keep Answerd prayers in portal & sort by status descending (to keep answered prayers at bottom)

--- church/church_prayers/doctype/church_prayer_request/church_prayer_request.py
+++ church/church_prayers/doctype/church_prayer_request/church_prayer_request.py
@@ -8,3 +8,9 @@ from frappe.website.website_generator import WebsiteGenerator
 
 class ChurchPrayerRequest(Document):
 	pass
+
+
+def get_list_context(context):
+	"""Sort the portal list view by status descenting"""
+	context.order_by = "status desc"
+	return context
