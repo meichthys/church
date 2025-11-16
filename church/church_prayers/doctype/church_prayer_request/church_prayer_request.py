49064ac006b733260d8651f9bd35b639ad86f94b
meichthys <github.com@meichthys.com>
2025-11-16 05:12:08 +0000

    sort prayer requests by modified date

--- church/church_prayers/doctype/church_prayer_request/church_prayer_request.py
+++ church/church_prayers/doctype/church_prayer_request/church_prayer_request.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, meichthys and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 from frappe.website.website_generator import WebsiteGenerator
 
@@ -11,6 +11,8 @@ class ChurchPrayerRequest(Document):
 
 
 def get_list_context(context):
-	"""Sort the portal list view by status descenting"""
-	context.order_by = "status desc"
+	# Only show documents created by active user
+	context.filters = {"owner": frappe.session.user}
+	# Sort the portal list view by status descending
+	context.order_by = "modified desc"
 	return context
