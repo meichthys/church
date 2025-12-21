1bc8886b0ee27ed37b0bbe337cf1996336fcf2b9
meichthys <github.com@meichthys.com>
2025-12-21 04:32:55 +0000

    Resolves #92

--- church/church_people/doctype/church_family/church_family.py
+++ church/church_people/doctype/church_family/church_family.py
@@ -21,3 +21,17 @@ class ChurchFamily(Document):
 			return
 		doc_dict[0]["doctype"] = "Church Person"
 		return frappe.get_doc(doc_dict[0]).name
+
+	def before_save(self):
+		# Remove family from Church Person records when Person is removed from ChurchFamily
+		if self.get_doc_before_save().members:
+			for member in self.get_doc_before_save().members:
+				if member not in self.members:
+					frappe.db.set_value("Church Person", member.member, "family", None, update_modified=False)
+		# Update Church Person records when ChurchFamily is updated
+		if self.members:
+			for member in self.members:
+				if member.member:
+					frappe.db.set_value(
+						"Church Person", member.member, "family", self.name, update_modified=False
+					)
