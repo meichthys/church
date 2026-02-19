eca9e7da01b8b67d99925f58bdc317360e68a4f4
meichthys <github.com@meichthys.com>
2026-02-19 05:58:42 +0000

    Add 'publish' or similar field to documents that are displayed on public website. Fixes #111

--- church/church_foundations/doctype/church_belief/church_belief.js
+++ church/church_foundations/doctype/church_belief/church_belief.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2025, meichthys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Church Belief", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Church Belief", {
+	refresh(frm) {
+        //Add an 'intro' indicating if a belief is published to the website
+        if(frm.doc.publish) {
+            frm.set_intro('🌐 This belief is published to the public website', 'blue');
+        }
+	},
+});
--- church/church_missions/doctype/church_missionary/church_missionary.js
+++ church/church_missions/doctype/church_missionary/church_missionary.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, meichthys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Church Missionary", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Church Missionary", {
+	refresh(frm) {
+        //Add an 'intro' indicating if a missionary is published to the website
+        if (frm.doc.sensitive && frm.doc.publish){
+            frm.set_intro('🚨 This missionary is marked as sensitive and is published \
+                to the public website. Make sure sensitive content is not being \
+                leaked to the public!', 'red')
+        }
+        else if(frm.doc.sensitive) {
+            frm.set_intro('⚠️ This missionary is marked as sensitive. \
+                be careful not to disclose sensitive information.', 'yellow');
+        }
+        else if(frm.doc.publish) {
+            frm.set_intro('🌐 This missionary is published to the public website', 'blue');
+        }
+	},
+});
