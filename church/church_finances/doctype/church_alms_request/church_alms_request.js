f355cc4f57eb7fb321c1d66d8fb269a3b34a9c01
meichthys <github.com@meichthys.com>
2026-02-20 06:00:32 +0000

    resolves Add `Requestor` field to Prayer/Alms requests Fixes #108

--- church/church_finances/doctype/church_alms_request/church_alms_request.js
+++ church/church_finances/doctype/church_alms_request/church_alms_request.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Church Alms Request', {
+    recipient_type: function(frm) {
+        // Dynamically update the Recipient field description based on the Recipient Type that is selected
+        if (frm.doc.recipient_type) {
+            frm.set_df_property('recipient', 'description', `Alms are requested for this ${frm.doc.recipient_type}.`);
+        }
+    },
     refresh(frm) {
         // Add a custom button that creates an Expense from the Alms Request
         if (!frm.is_new()) {
@@ -18,6 +24,17 @@ frappe.ui.form.on('Church Alms Request', {
             });
         }
     },
+    onload: function(frm) {
+        // Pre-populate the requestor field with the current user's name
+        if (frm.is_new()) {
+            frappe.db.get_value('Church Person', {'portal_user': frappe.session.user}, 'name')
+                .then(r => {
+                    if (r && r.message) {
+                        frm.set_value('requestor', r.message.name);
+                    }
+                });
+        }
+    }
 });
 
 
--- church/church_prayers/doctype/church_prayer_request/church_prayer_request.js
+++ church/church_prayers/doctype/church_prayer_request/church_prayer_request.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2025, meichthys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Church Prayer Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Church Prayer Request', {
+    onload: function(frm) {
+        // Pre-populate the requestor field with the current user's name
+        if (frm.is_new()) {
+            frappe.db.get_value('Church Person', {'portal_user': frappe.session.user}, 'name')
+                .then(r => {
+                    if (r && r.message) {
+                        frm.set_value('requestor', r.message.name);
+                    }
+                });
+        }
+    }
+});
