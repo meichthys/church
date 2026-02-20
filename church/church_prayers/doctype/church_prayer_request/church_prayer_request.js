72a3b0b23380e5fd0b87a10e3be51425cb62c1b2
meichthys <github.com@meichthys.com>
2026-02-20 06:00:50 +0000

    Resolves Add `Requestor` field to Prayer/Alms requests Fixes #108

--- church/church_prayers/doctype/church_prayer_request/church_prayer_request.js
+++ church/church_prayers/doctype/church_prayer_request/church_prayer_request.js
@@ -2,6 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Church Prayer Request', {
+    recipient_type: function(frm) {
+        // Dynamically update the Recipient field description based on the Recipient Type that is selected
+        if (frm.doc.recipient_type) {
+            frm.set_df_property('recipient', 'description', `Prayers are requested for this ${frm.doc.recipient_type}.`);
+        }
+    },
     onload: function(frm) {
         // Pre-populate the requestor field with the current user's name
         if (frm.is_new()) {
