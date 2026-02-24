aada1f9e90752ceb7fcf6cf687ad2efdeadc7e55
meichthys <github.com@meichthys.com>
2026-02-24 03:32:09 +0000

    format app_include_js

--- church/hooks.py
+++ church/hooks.py
@@ -248,7 +248,9 @@ fixtures = [
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/church/css/church.css"
-app_include_js = "/assets/church/js/help_icon_on_form.js"
+app_include_js = [
+	"/assets/church/js/help_icon_on_form.js",
+]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/church/css/church.css"
