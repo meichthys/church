53f2ab83ce7781f1142b2e69ad8fa7b820ebfefb
meichthys <github.com@meichthys.com>
2026-03-09 05:07:47 +0000

    limit to only one 'root' church

--- church/church_foundations/doctype/church/church.py
+++ church/church_foundations/doctype/church/church.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2025, meichthys and contributors
 # For license information, please see license.txt
 
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Church(Document):
-	pass
+	def validate(self):
+		if not self.parent_church:
+			existing_root = frappe.db.get_value(
+				"Church",
+				{"parent_church": ("is", "not set"), "name": ("!=", self.name)},
+				"name",
+			)
+			if existing_root:
+				frappe.throw(
+					_("Only one root Church is allowed. '{0}' is already the root church. Please set a Parent Church.").format(
+						existing_root
+					)
+				)
