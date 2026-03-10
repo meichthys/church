7c461a2c8c405055149f811f19aefd327fbb292b
meichthys <github.com@meichthys.com>
2026-03-10 03:38:19 +0000

    pre-populate church in Church Directory based on user

--- church/church_people/report/church_directory_report/church_directory_report.js
+++ church/church_people/report/church_directory_report/church_directory_report.js
@@ -61,13 +61,15 @@ frappe.query_reports["Church Directory Report"] = {
 			fieldtype: "Check",
 			default: 0,
 		},
+
 	],
 
 	onload: function (report) {
-		$('<div class="alert alert-info small" style="margin: var(--margin-sm);">' +
-			'Tip: You can print this directory as a booklet: ' +
-			'<a href="https://helpx.adobe.com/acrobat/kb/print-booklets-acrobat-reader.html" target="_blank">Learn how to print as a booklet</a>.' +
-		'</div>').prependTo(report.page.main);
+		const default_church = frappe.defaults.get_user_default("church");
+		if (default_church) {
+			report.set_filter_value("church", default_church);
+		}
+
 		report.page.add_inner_button(__('Print Directory'), function () {
 			const church = report.get_filter_value('church');
 			const include_sub_churches = report.get_filter_value('include_sub_churches') ? 1 : 0;
